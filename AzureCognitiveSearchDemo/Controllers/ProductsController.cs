a2be203c67f709e6c029cb4a30567732ed35e67e
Francisco-pc <francisco.florio25@gmail.com>
2023-07-26 01:10:34 -0300

    Borrado de objetos innecesarios

--- AzureCognitiveSearchDemo/Controllers/ProductsController.cs
+++ AzureCognitiveSearchDemo/Controllers/ProductsController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AzureCognitiveSearchDemo.Controllers
 {
--- AzureCognitiveSearchDemo/DTO/ResultsDTO.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace AzureCognitiveSearchDemo.DTO
-{
-    public record ResultsDTO(string Name, string Description, DateTime creation);
-}
